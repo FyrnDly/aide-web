be6f740bb534e3468a27bf138b838f486108a42e
FyrnDly <fadramdani25@gmail.com>
2024-05-17 23:09:13 +0700

    [Add] Root Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\RootMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'admin' => AdminMiddleware::class,
+            'root' => RootMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
