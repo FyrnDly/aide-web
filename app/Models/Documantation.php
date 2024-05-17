188053c2584ebada22b4f055e53647e8af257b29
FyrnDly <fadramdani25@gmail.com>
2024-05-17 22:59:34 +0700

    [Fix] Migration Table Name Documantation

--- app/Models/Documantation.php
+++ app/Models/Documantation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Documentation extends Model
+class Documantation extends Model
 {
     use HasFactory;
 
--- database/migrations/2024_05_17_092249_create_documantations_table.php
+++ database/migrations/2024_05_17_092249_create_documantations_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('documentations', function (Blueprint $table) {
+        Schema::create('documantations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->date('date');
             $table->string('path');
             $table->string('description');
             $table->timestamps();
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('documentations');
+        Schema::dropIfExists('documantations');
     }
 };
