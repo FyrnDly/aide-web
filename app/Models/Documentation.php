10c59844fa9c6e10ee007bd6c2d61cc4c94e5a32
FyrnDly <fadramdani25@gmail.com>
2024-05-19 03:29:13 +0700

    [Add] Admin View Documentation

--- app/Models/Documentation.php
+++ app/Models/Documentation.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Documantation extends Model
+class Documentation extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'name',
+        'date',
         'path',
         'description',
     ];
