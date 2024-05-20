9af21023ec3639d69d795521f952d57073f90b40
FyrnDly <fadramdani25@gmail.com>
2024-05-20 11:04:55 +0700

    [Fix] Fix double params for query paginator

--- resources/views/components/loadmore.blade.php
+++ resources/views/components/loadmore.blade.php
@@ -1,17 +1,24 @@
-@if($paginator->count() < $paginator->total())
-    @php
-    $url = url()->full();
-    $arrUrl = parse_url($url);
-    if (isset($arrUrl['query'])) {
-    $urlNextPage = $url.'&'.$paginator->getPageName().'='.($paginator->count()+1);
-    } else {
-    $urlNextPage = $url.'?'.$paginator->getPageName().'='.($paginator->count()+1);
-    }
-    @endphp
-    <div class="text-center my-2">
-        <button onclick="window.location.href='{{ $urlNextPage }}'" class="btn btn-primary">
-            Tampilkan Lebih Banyak
-        </button>
-    </div>
-    @endif
+@if ($paginator->count() < $paginator->total())
+@php
+$url = url()->full();
+$urlPage = url()->current();
+$arrUrl = parse_url($url);
+$paginatorName = $paginator->getPageName();
 
+if (isset($arrUrl['query'])) {
+parse_str($arrUrl['query'], $queryParams);
+    if (isset($queryParams[$paginatorName])) {
+        unset($queryParams[$paginatorName]);
+    }
+    $urlParams = http_build_query($queryParams);
+    $urlNextPage = $urlPage.'?'.$urlParams.'&'.$paginatorName.'='.($paginator->count() + 1);
+} else{
+    $urlNextPage = $urlPage.'?'.$paginatorName.'='.($paginator->count() + 1);
+}
+@endphp
+<div class="text-center my-2">
+    <button onclick="window.location.href='{{ $urlNextPage }}'" class="btn btn-primary">
+        Tampilkan Lebih Banyak
+    </button>
+</div>
+@endif
